cec4b71ab3cb0785f017a38ab1bd90a768f07416
OliverCulleyDeLange <olly@oliverdelange.co.uk>
2020-06-14 17:37:01 +0100

    Add changing notification icon

--- app/src/main/java/uk/co/oliverdelange/flicify/service/FlicifyService.kt
+++ app/src/main/java/uk/co/oliverdelange/flicify/service/FlicifyService.kt
@@ -1,16 +1,17 @@
 package uk.co.oliverdelange.flicify.service
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.app.Service
+import android.app.*
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.IBinder
+import android.util.Log
 import androidx.core.app.NotificationCompat
+import io.reactivex.rxkotlin.ofType
 import uk.co.oliverdelange.flicify.R
+import uk.co.oliverdelange.flicify.redux.AppStore
+import uk.co.oliverdelange.flicify.redux.Event
 import uk.co.oliverdelange.flicify.view.FlicActivity
 
 class FlicifyService : Service() {
@@ -19,26 +20,38 @@ class FlicifyService : Service() {
 
     override fun onCreate() {
         super.onCreate()
-        val notificationIntent = Intent(this, FlicActivity::class.java)
-        val contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val mChannel = NotificationChannel(
                 NOTIFICATION_CHANNEL_ID,
                 NOTIFICATION_CHANNEL_NAME,
                 NotificationManager.IMPORTANCE_LOW
             )
-            val notificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(mChannel)
         }
-        val notification = NotificationCompat.Builder(this.applicationContext, NOTIFICATION_CHANNEL_ID)
+        startForeground(SERVICE_NOTIFICATION_ID, getNotification(false))
+
+        AppStore.actions.ofType<Event.Flic>().doOnNext {
+            Log.w("BG", "Flic event: $it")
+            notificationManager.notify(SERVICE_NOTIFICATION_ID, getNotification(it.isDown))
+        }.subscribe()
+    }
+
+    private fun getNotification(down: Boolean): Notification? =
+        NotificationCompat.Builder(applicationContext, NOTIFICATION_CHANNEL_ID)
             .setContentTitle("Flicify")
             .setContentText("Flic -> Spotify")
-            .setSmallIcon(R.mipmap.ic_launcher)
-            .setContentIntent(contentIntent)
+            .setSmallIcon(if (down) R.drawable.ic_notification_pressed else R.drawable.ic_notification)
+            .setContentIntent(
+                PendingIntent.getActivity(
+                    this,
+                    0,
+                    Intent(this, FlicActivity::class.java),
+                    PendingIntent.FLAG_CANCEL_CURRENT
+                )
+            )
             .setOngoing(true)
             .build()
-        startForeground(SERVICE_NOTIFICATION_ID, notification)
-    }
 
     override fun onBind(intent: Intent): IBinder? {
         throw UnsupportedOperationException("Not implemented")
@@ -46,12 +59,14 @@ class FlicifyService : Service() {
 
     class BootUpReceiver : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
+            Log.w("APP", "Device restarted")
             // The Application class's onCreate has already been called at this point, which is what we want
         }
     }
 
     class UpdateReceiver : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
+            Log.w("APP", "App re-installed / updated")
             // The Application class's onCreate has already been called at this point, which is what we want
         }
     }
