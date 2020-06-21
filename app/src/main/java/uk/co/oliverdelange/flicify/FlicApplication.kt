d95bb3a936f1679182a79cca25a27911cffcfb57
OliverCulleyDeLange <olly@oliverdelange.co.uk>
2020-06-21 13:44:27 +0100

    Remove logging in prod

--- app/src/main/java/uk/co/oliverdelange/flicify/FlicApplication.kt
+++ app/src/main/java/uk/co/oliverdelange/flicify/FlicApplication.kt
@@ -11,9 +11,9 @@ import uk.co.oliverdelange.flicify.service.FlicifyService
 class Flicify : Application() {
     override fun onCreate() {
         super.onCreate()
-//        if (BuildConfig.DEBUG) {
+        if (BuildConfig.DEBUG) {
             Timber.plant(CustomDebugTree())
-//        }
+        }
 
         Timber.v("onCreate Flicify Application")
 
